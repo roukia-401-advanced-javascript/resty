adc1fe709be4c48d19b569aef728cbe603e3e274
roukiaSalahi <roqaia.sa@hotmail.com>
2020-10-15 01:19:15 +0300

    composition

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 import Form from './components/form/form';
 import Footer from './components/footer/footer';
 import Header from './components/header/header.js';
 import Results from './components/results/results.js';
-import History from './components/history/history';
+
+import If from './components/if/if';
+
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      count:0,
-      results: [],
-      headers: [],
-      // results: null,
-      // headers:'',
+      count: 0,
+      results: null, //data results
+      headers: null,
+      loading: false,
+
     };
+  }
+
+  toggleLoading = () => {
+    this.setState({ loading: !this.state.loading });
+    console.log('loading in toggleLoading app.js....', this.state.loading)
   }
   // url.......  https://swapi.dev/api/people/
 
   // came from child form with parameters url and option 
-  handelUpdate = async (url,options)=> {
-    // console.log('url from app.js ', url);
-    let raw = await fetch(url,options);
-    console.log("this is raaaw".raw)
-    let data = await raw.json();
-    console.log("this is data after fetch",data)
-    console.log("this is raaaw.headeeers",raw.headers)
-    let headers={};
-    raw.headers.forEach( (value, key) => {
-      headers[key] = value;
+  handelUpdate = (count, results, headers) => {
+    this.setState({ count, results, headers });
 
-    });
-    
-    this.setState({headers: headers, results: data.results, count: data.count });
   };
-
- 
   render() {
     return (
       <React.Fragment>
         <Header />
-        <Form handelUpdate={this.handelUpdate}/>
-        <Results headers={this.state.headers} count={this.state.count} results={this.state.results} />
-        {/* // i can use anything inside handleupdate in the history child by the props */}
-        <History fetchData={this.handelUpdate}/> 
+        <Form loading={this.state.loading} handelUpdate={this.handelUpdate.bind(this)} toggle={this.toggleLoading.bind(this)} />
+        {/* <If condition={this.state.results}> */}
+        <Results loading={this.state.loading} headers={this.state.headers} count={this.state.count} results={this.state.results} />
+        {/* </If> */}
         <Footer />
       </React.Fragment>
     );
@@ -54,4 +47,5 @@ class App extends React.Component {
 }
 
 export default App;
-//github
+
+
